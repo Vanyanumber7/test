a238dff366317e85a99e7cb6a150c8eb3a5596fe
Vanyanumber7 <76075240+Vanyanumber7@users.noreply.github.com>
2021-03-24 18:22:45 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+
+from flask import Flask
+from waitress import serve
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Привет от приложения Flask"
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    # app.run(port=port, host="0.0.0.0")
+
+    # с дефаултными значениями будет не более 4 потов
+    serve(app, port=port, host="0.0.0.0")
